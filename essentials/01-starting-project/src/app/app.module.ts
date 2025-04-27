73abb3e4e17c0b591d4a3366b382d1989f228ae1
Vinod <vkm041507@gmail.com>
2025-04-27 09:12:48 +0530

    initial check

--- essentials/01-starting-project/src/app/app.module.ts
+++ essentials/01-starting-project/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { SharedModule } from "./shared/shared.module";
 import { UserComponent } from "./user/user.component";
 import { TasksModule } from "./tasks/tasks.module";
 import { HeaderComponent } from "./header/header.conponent";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
     declarations: [AppComponent,UserComponent,HeaderComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule,SharedModule,TasksModule],
+    imports: [BrowserModule,SharedModule,TasksModule,FormsModule],
 })
 export class AppModule{
 
